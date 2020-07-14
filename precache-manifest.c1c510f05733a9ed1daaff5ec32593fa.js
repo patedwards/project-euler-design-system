c1be35d314f0c4220c00a9158efb5f012eba75b9
patwards <patrick.edwards@biarri.com>
2020-07-13 20:03:51 -0700

    Updates

--- precache-manifest.c1c510f05733a9ed1daaff5ec32593fa.js
+++ precache-manifest.c1c510f05733a9ed1daaff5ec32593fa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e8f6063d34640c0f1198d8b06ab74c01",
+    "revision": "bad80b1b0f39db875dc3ca8c832b9492",
     "url": "/project-norman/index.html"
   },
   {
-    "revision": "eed61aa1317aed8fb827",
+    "revision": "e84bf45f2f5eb6d249f1",
     "url": "/project-norman/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "8819ac987d32c7d05aa5",
-    "url": "/project-norman/static/js/2.5426be32.chunk.js"
+    "revision": "1369e03cdcc24201040a",
+    "url": "/project-norman/static/js/2.38df5168.chunk.js"
   },
   {
-    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
-    "url": "/project-norman/static/js/2.5426be32.chunk.js.LICENSE.txt"
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-norman/static/js/2.38df5168.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eed61aa1317aed8fb827",
-    "url": "/project-norman/static/js/main.0a56ed56.chunk.js"
+    "revision": "e84bf45f2f5eb6d249f1",
+    "url": "/project-norman/static/js/main.7d4e0a4f.chunk.js"
   },
   {
     "revision": "6d6de50312f36dd8e2e2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-norman/precache-manifest.f7e0866531e1c0092a0771a141b8b45b.js"
+  "/project-norman/precache-manifest.c1c510f05733a9ed1daaff5ec32593fa.js"
 );
 
 self.addEventListener('message', (event) => {
